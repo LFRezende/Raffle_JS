f1e94e1b3f7091e7eb1e2e82edf996c90cbd20ae
LFRezende <lfsrsprofessional@gmail.com>
2023-08-07 21:38:39 -0300

    Update: Smart Contract Finished and Compiling!

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -22,6 +22,11 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 error Raffle__NotEnoughETHEntered(); // Error msg is better than storing strings
 error Raffle__TransferFailed();
 error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(
+    uint256 currentBalance,
+    uint256 numPlayers,
+    uint256 raffleState
+);
 
 abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     // ENUMS
@@ -47,6 +52,8 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
     // Lottery Variables
     address private s_recentWinner;
     raffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
     // Events of the Contract
     event raffleEnter(address indexed player); // indexed = event indexed is easier to query (less gas)
     event requestedRaffleWinner(uint256 indexed requestId);
@@ -57,7 +64,8 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         uint256 entranceFee,
         bytes32 gasLane, // equal to keyHash
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorAddress) {
         // inherits the constructor since it inherits the functions of VRFConsumerBaseV2
         i_entranceFee = entranceFee;
@@ -66,6 +74,8 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
         s_raffleState = raffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     function enterRaffle() public payable {
@@ -79,11 +89,25 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         emit raffleEnter(msg.sender);
     }
 
-    function requestRandomWinner() external {
+    /**@dev
+     * This function was the original requestRandomWinner, but reworked from
+     * Chainlink Keepers -> IT will request the randomWinner, therefore, IT will run all
+     * of these.
+     */
+    function performUpkeep(bytes calldata /* performData*/) external override {
         // External function less gas
         // Request random
         // Do something
         // 2 tx;
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        s_raffleState = raffleState.CALCULATING;
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        }
         i_vrfCoordinator.requestRandomWords(
             i_gasLane, // maximum amount of gas we are willing to pay for the random number
             i_subscriptionId, // your subscription ID, on chainlink VRF, to fund ans request
@@ -102,6 +126,10 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_raffleState = raffleState.OPEN;
+        // Reseting the list of players
+        s_players = new address payable[](0); // reset the players after the end of raffle
+        s_lastTimeStamp = block.timestamp; // Reset the timestamp
         (bool success, ) = recentWinner.call{value: address(this).balance}("");
         if (!success) {
             revert Raffle__TransferFailed();
@@ -117,9 +145,20 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
      * 4. Must be open
      */
 
+    // Alteramos para memory pois mais pra frente passamos um "" como bytes
     function checkUpkeep(
-        bytes calldata /*checkData*/
-    ) external override returns (bool upkeepNeeded, bytes memory performData) {}
+        bytes memory /*checkData*/ // set to public because our own contract will also call it
+    )
+        public
+        override
+        returns (bool upkeepNeeded, bytes memory /*performData*/)
+    {
+        bool isOpen = (raffleState.OPEN == s_raffleState);
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = (s_players.length > 0);
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
@@ -131,5 +170,26 @@ abstract contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
 
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
+    }
+
+    function getRaffleState() public view returns (raffleState) {
+        return s_raffleState;
+    }
+
+    // Super interesting! Restrict it to pure! It's not reading from the chain. It's reading from the contract!
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getLatestTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
     }
 }
